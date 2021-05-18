6610335df19b62fd3e85709ce18996ca071a85f2
Glitchygamerboi <mayuukhb.student@mlsi.in>
2021-05-18 17:10:59 +0530

    ignore the errors, just added S A N D

--- src/main/java/net/glitchyystudios/better_underground/registry/BlockRegistry.java
+++ src/main/java/net/glitchyystudios/better_underground/registry/BlockRegistry.java
@@ -20,7 +20,7 @@ public static final Block ALKALI_FELDSPAR_GRANITE = new Block(FabricBlockSetting
 public static final Block RHYODACITE =new Block(FabricBlockSettings.copy(Blocks.GRANITE).requiresTool().strength(1.5F, 6.0F));
 public static final Block NEPHELINE = new Block(FabricBlockSettings.copy(Blocks.GRANITE).requiresTool().strength(1.5F, 6.0F));
 public static final Block GREY_DACITE = new Block(FabricBlockSettings.copy(Blocks.GRANITE).requiresTool().strength(1.5F, 6.0F));
-public static final Block STONE_SAND = new Block(FabricBlockSettings.copy(Blocks.SAND.requiresTool()))
+public static final Block STONE_SAND = new Block(FabricBlockSettings.copy(Blocks.SAND).requiresTool().strength(1.5F, 5.5F));
 //Follow the template below if you want to contribute a block, im going to create the BlockItem
 public static final Block EXAMPLE_BLOCK = new Block(FabricBlockSettings.copy(Blocks.GRANITE).requiresTool().strength(0F, 0F));
     public static void register() {
@@ -34,6 +34,7 @@ public static final Block EXAMPLE_BLOCK = new Block(FabricBlockSettings.copy(Blo
         Registry.register(Registry.BLOCK, Utils.ID("rhyodacite"), RHYODACITE);
         Registry.register(Registry.BLOCK, Utils.ID("nepheline"), NEPHELINE);
         Registry.register(Registry.BLOCK, Utils.ID("grey_dacite"), GREY_DACITE);
+        Registry.register(Registry.BLOCK, Utils.ID("stone_sand"), STONE_SAND);
         //Follow the template below to register an block
         Registry.register(Registry.BLOCK, Utils.ID("example_block"), EXAMPLE_BLOCK);
     }
---
+++ src/main/java/net/glitchyystudios/better_underground/worldgen/CustomConfiguredFeatures.java
@@ -0,0 +1,57 @@
+package net.glitchyystudios.better_underground.worldgen;
+
+import net.minecraft.world.gen.HeightContext;
+import net.minecraft.world.gen.decorator.Decorator;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.glitchyystudios.better_underground.Utils;
+import net.glitchyystudios.better_underground.registry.BlockRegistry;
+import net.minecraft.util.registry.BuiltinRegistries;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.YOffset;
+import net.minecraft.world.gen.decorator.RangeDecoratorConfig;
+import net.minecraft.world.gen.feature.*;
+import net.minecraft.world.gen.heightprovider.HeightProvider;
+import net.minecraft.world.gen.heightprovider.HeightProviderType;
+
+import java.util.Random;
+
+public class CustomConfiguredFeatures {
+
+    private static ConfiguredFeature<?, ?> ORE_COMPRESSED_STONE = Feature.ORE.configure(new OreFeatureConfig(OreFeatureConfig.Rules.BASE_STONE_OVERWORLD, BlockRegistry.COMPRESSED_STONE.getDefaultState(), 33)).method_36296(YOffset.fixed(0), YOffset.fixed(16)).spreadHorizontally().repeat(10);
+    private static ConfiguredFeature<?, ?> ORE_RHYOLITE = Feature.ORE.configure(new OreFeatureConfig(OreFeatureConfig.Rules.BASE_STONE_OVERWORLD, BlockRegistry.RHYOLITE.getDefaultState(), 33)).method_36296(YOffset.fixed(0), YOffset.fixed(200)).spreadHorizontally().repeat(16);
+    private static ConfiguredFeature<?, ?> ORE_MARBLE = Feature.ORE.configure(new OreFeatureConfig(OreFeatureConfig.Rules.BASE_STONE_OVERWORLD, BlockRegistry.MARBLE.getDefaultState(), 33)).method_36296(YOffset.fixed(0), YOffset.fixed(200)).spreadHorizontally().repeat(16);
+    private static ConfiguredFeature<?, ?> ORE_DOLOMITE = Feature.ORE.configure(new OreFeatureConfig(OreFeatureConfig.Rules.BASE_STONE_OVERWORLD, BlockRegistry.DOLOMITE.getDefaultState(), 33)).method_36296(YOffset.fixed(0), YOffset.fixed(16)).spreadHorizontally().repeat(16);
+    private static ConfiguredFeature<?, ?> ORE_LIMESTONE = Feature.ORE.configure(new OreFeatureConfig(OreFeatureConfig.Rules.BASE_STONE_OVERWORLD, BlockRegistry.LIMESTONE.getDefaultState(), 33)).method_36296(YOffset.fixed(0), YOffset.fixed(200)).spreadHorizontally().repeat(16);
+    private static ConfiguredFeature<?, ?> ORE_LAKE_MISSOULA_CLAYSTONE_SEDIMENT = Feature.ORE.configure(new OreFeatureConfig(OreFeatureConfig.Rules.BASE_STONE_OVERWORLD, BlockRegistry.LAKE_MISSOULA_CLAYSTONE_SEDIMENT.getDefaultState(), 33)).method_36296(YOffset.fixed(0), YOffset.fixed(200)).spreadHorizontally().repeat(16);
+    private static ConfiguredFeature<?, ?> ORE_ALKALI_FELDSPAR_GRANITE = Feature.ORE.configure(new OreFeatureConfig(OreFeatureConfig.Rules.BASE_STONE_OVERWORLD, BlockRegistry.ALKALI_FELDSPAR_GRANITE.getDefaultState(), 33)).method_36296(YOffset.fixed(0), YOffset.fixed(200)).spreadHorizontally().repeat(16);
+    private static ConfiguredFeature<?, ?> ORE_RHYODACITE = Feature.ORE.configure(new OreFeatureConfig(OreFeatureConfig.Rules.BASE_STONE_OVERWORLD, BlockRegistry.RHYODACITE.getDefaultState(), 33)).method_36296(YOffset.fixed(0), YOffset.fixed(200)).spreadHorizontally().repeat(16);
+    private static ConfiguredFeature<?, ?> ORE_NEPHELINE = Feature.ORE.configure(new OreFeatureConfig(OreFeatureConfig.Rules.BASE_STONE_OVERWORLD, BlockRegistry.NEPHELINE.getDefaultState(), 33)).method_36296(YOffset.fixed(0), YOffset.fixed(200)).spreadHorizontally().repeat(16);
+    private static ConfiguredFeature<?, ?> ORE_GREY_DACITE = Feature.ORE.configure(new OreFeatureConfig(OreFeatureConfig.Rules.BASE_STONE_OVERWORLD, BlockRegistry.GREY_DACITE.getDefaultState(), 33))
+
+
+
+
+        public void register() {
+            registerOre("compressed_stone", ORE_COMPRESSED_STONE);
+            registerOre("rhyolite", ORE_RHYOLITE);
+            registerOre("marble", ORE_MARBLE);
+            registerOre("dolomite", ORE_DOLOMITE);
+            registerOre("limestone", ORE_LIMESTONE);
+            registerOre("lake_missoula_claystone_sediment", ORE_LAKE_MISSOULA_CLAYSTONE_SEDIMENT);
+            registerOre("alkali_feldspar_granite", ORE_ALKALI_FELDSPAR_GRANITE);
+            registerOre("rhyodacite", ORE_RHYODACITE);
+            registerOre("nepheline", ORE_NEPHELINE);
+            registerOre("grey_dacite", ORE_GREY_DACITE);
+
+
+        }
+
+        private void registerOre(String id, ConfiguredFeature<?, ?> ORE) {
+            RegistryKey<ConfiguredFeature<?, ?>> ore = RegistryKey.of(Registry.CONFIGURED_FEATURE_KEY, Utils.ID(id));
+            Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, ore.getValue(), ORE);
+            BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, ore);
+        }
+    }
