cdda79d1f04b53bcc8321a22bfe8c21c22c22906
Glitchygamerboi <mayuukhb.student@mlsi.in>
2021-05-31 15:21:38 +0530

    replaced rhyolite wastes with boreal plain

--- src/main/java/net/glitchyystudios/better_underground/worldgen/biome.java
+++ src/main/java/net/glitchyystudios/better_underground/worldgen/biome.java
@@ -14,18 +14,36 @@ import net.minecraft.world.gen.surfacebuilder.SurfaceBuilder;
 import net.minecraft.world.gen.surfacebuilder.TernarySurfaceConfig;
 
 public class biome {
-    private static final ConfiguredSurfaceBuilder<TernarySurfaceConfig> RWASTES_SURFACE_BUILDER = SurfaceBuilder.DEFAULT.withConfig(new TernarySurfaceConfig(BlockRegistry.RHYOLITE.getDefaultState(), BlockRegistry.RHYODACITE.getDefaultState(), BlockRegistry.NEPHELINE.getDefaultState()));private static final Biome RWASTES = createRwastes();private static Biome createRwastes() {// We specify what entities spawn and what features generate in the biome// Aside from some structures, trees, rocks, plants and//   custom entities, these are mostly the same for each biome.// Vanilla configured features for biomes are defined in DefaultBiomeFeatures
-        SpawnSettings.Builder spawnSettings = new SpawnSettings.Builder();DefaultBiomeFeatures.addFarmAnimals(spawnSettings);DefaultBiomeFeatures.addMonsters(spawnSettings, 95, 5, 100);GenerationSettings.Builder generationSettings = new GenerationSettings.Builder();generationSettings.surfaceBuilder(RWASTES_SURFACE_BUILDER);DefaultBiomeFeatures.addDefaultUndergroundStructures(generationSettings);DefaultBiomeFeatures.addLandCarvers(generationSettings);DefaultBiomeFeatures.addDefaultLakes(generationSettings);DefaultBiomeFeatures.addDungeons(generationSettings);DefaultBiomeFeatures.addMineables(generationSettings);DefaultBiomeFeatures.addDefaultOres(generationSettings);DefaultBiomeFeatures.addDefaultDisks(generationSettings);DefaultBiomeFeatures.addSprings(generationSettings);DefaultBiomeFeatures.addFrozenTopLayer(generationSettings);return (new Biome.Builder()).precipitation(Biome.Precipitation.RAIN).category(Biome.Category.NONE).depth(0.125F).scale(0.05F).temperature(0.8F).downfall(0.4F).effects((new BiomeEffects.Builder()).waterColor(0x3f76e4).waterFogColor(0x050533).fogColor(0xc0d8ff).skyColor(0x77adff).build()).spawnSettings(spawnSettings.build()).generationSettings(generationSettings.build()).build();
+    private static final ConfiguredSurfaceBuilder<TernarySurfaceConfig> BOREAL_SURFACE_BUILDER = SurfaceBuilder.DEFAULT.withConfig(new TernarySurfaceConfig(BlockRegistry.BOREAL_DIRT.getDefaultState(), BlockRegistry.RHYODACITE.getDefaultState(), BlockRegistry.NEPHELINE.getDefaultState()));private static final Biome RWASTES = createRwastes();private static Biome createRwastes() {// We specify what entities spawn and what features generate in the biome// Aside from some structures, trees, rocks, plants and//   custom entities, these are mostly the same for each biome.// Vanilla configured features for biomes are defined in DefaultBiomeFeatures
+        
+        SpawnSettings.Builder spawnSettings = new SpawnSettings.Builder();
+        DefaultBiomeFeatures.addFarmAnimals(spawnSettings);
+        DefaultBiomeFeatures.addMonsters(spawnSettings, 95, 5, 100);
+        GenerationSettings.Builder generationSettings = new GenerationSettings.Builder();
+        generationSettings.surfaceBuilder(BOREAL_SURFACE_BUILDER);
+        DefaultBiomeFeatures.addDefaultUndergroundStructures(generationSettings);
+        DefaultBiomeFeatures.addLandCarvers(generationSettings);
+        DefaultBiomeFeatures.addDefaultLakes(generationSettings);
+        DefaultBiomeFeatures.addDungeons(generationSettings);
+        DefaultBiomeFeatures.addMineables(generationSettings);
+        DefaultBiomeFeatures.addDefaultOres(generationSettings);
+        DefaultBiomeFeatures.addDefaultDisks(generationSettings);
+        DefaultBiomeFeatures.addSprings(generationSettings);
+        DefaultBiomeFeatures.addFrozenTopLayer(generationSettings);
+        DefaultBiomeFeatures.addTaigaTrees(generationSettings);
+        DefaultBiomeFeatures.addMossyRocks(generationSettings);
+        return (new Biome.Builder()).precipitation(Biome.Precipitation.RAIN).category(Biome.Category.NONE).depth(0.125F).scale(0.05F).temperature(0.8F).downfall(0.4F).effects((new BiomeEffects.Builder()).waterColor(0x3f76e4).waterFogColor(0x050533).fogColor(0xc0d8ff).skyColor(0x77adff).build()).spawnSettings(spawnSettings.build()).generationSettings(generationSettings.build()).build();
     }
 
 
-    public static final RegistryKey<Biome> RWASTES_KEY = RegistryKey.of(Registry.BIOME_KEY, Utils.ID("rhyolite_wastes"));
+
+    public static final RegistryKey<Biome> BOREAL_KEY = RegistryKey.of(Registry.BIOME_KEY, Utils.ID("boreal_plain"));
 
     public static void register() {
-        Registry.register(BuiltinRegistries.CONFIGURED_SURFACE_BUILDER, Utils.ID("rhyolite_wastes"), RWASTES_SURFACE_BUILDER);
-        Registry.register(BuiltinRegistries.BIOME, RWASTES_KEY.getValue(), RWASTES);
-        OverworldBiomes.addContinentalBiome(RWASTES_KEY, OverworldClimate.COOL, 50);
-        OverworldBiomes.addContinentalBiome(RWASTES_KEY, OverworldClimate.SNOWY, 50);
+        Registry.register(BuiltinRegistries.CONFIGURED_SURFACE_BUILDER, Utils.ID("boreal_plain"), BOREAL_SURFACE_BUILDER);
+        Registry.register(BuiltinRegistries.BIOME, BOREAL_KEY.getValue(), RWASTES);
+        OverworldBiomes.addContinentalBiome(BOREAL_KEY, OverworldClimate.COOL, 50);
+        OverworldBiomes.addContinentalBiome(BOREAL_KEY, OverworldClimate.SNOWY, 50);
     }
 }
 
