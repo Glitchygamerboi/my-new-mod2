72862acac69fb71de1c173788194bd127f645855
Glitchygamerboi <mayuukhb.student@mlsi.in>
2021-07-06 13:27:24 +0530

    pebbles, AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- src/main/java/net/glitchyystudios/better_underground/registry/BlockRegistry.java
+++ src/main/java/net/glitchyystudios/better_underground/registry/BlockRegistry.java
@@ -10,25 +10,42 @@ public class BlockRegistry {
 
     private BlockRegistry(){}
 
-    public static final Block COMPRESSED_STONE = new Block(FabricBlockSettings.copy(Blocks.DEEPSLATE).requiresTool());
-    public static final Block RHYOLITE = new Block(FabricBlockSettings.copy(Blocks.GRANITE).requiresTool());
-    public static final Block MARBLE = new Block(FabricBlockSettings.copy(Blocks.GRANITE).requiresTool());
-    public static final Block DOLOMITE = new Block(FabricBlockSettings.copy(Blocks.DEEPSLATE).requiresTool());
-    public static final Block LIMESTONE = new Block(FabricBlockSettings.copy(Blocks.GRANITE).requiresTool());
-    public static final Block LAKE_MISSOULA_CLAYSTONE_SEDIMENT = new Block(FabricBlockSettings.copy(Blocks.GRANITE).requiresTool());
-public static final Block ALKALI_FELDSPAR_GRANITE = new Block(FabricBlockSettings.copy(Blocks.GRANITE).requiresTool());
-public static final Block RHYODACITE =new Block(FabricBlockSettings.copy(Blocks.GRANITE).requiresTool());
-public static final Block NEPHELINE = new Block(FabricBlockSettings.copy(Blocks.GRANITE).requiresTool());
-public static final Block GREY_DACITE = new Block(FabricBlockSettings.copy(Blocks.GRANITE).requiresTool());
-public static final Block STONE_SAND = new Block(FabricBlockSettings.copy(Blocks.SAND).requiresTool());
-public static final Block BOREAL_DIRT = new Block(FabricBlockSettings.copy(Blocks.DIRT).requiresTool());
-public static final Block PEARL_DACITE = new Block(FabricBlockSettings.copy(Blocks.GRANITE).requiresTool());
-public static final Block LAYERSTONE = new Block(FabricBlockSettings.copy(Blocks.STONE).requiresTool());
-    public static final Block GOLD_ORE_LAYERSTONE = new Block(FabricBlockSettings.copy(Blocks.STONE).requiresTool());
-    public static final Block PUMICE = new Block(FabricBlockSettings.copy(Blocks.STONE).requiresTool());
-    public static final Block GRANODIORITE = new Block(FabricBlockSettings.copy(Blocks.STONE).requiresTool());
+    public static final Block COMPRESSED_STONE = new Block(FabricBlockSettings.copy(Blocks.DEEPSLATE));
+    public static final Block RHYOLITE = new Block(FabricBlockSettings.copy(Blocks.GRANITE));
+    public static final Block MARBLE = new Block(FabricBlockSettings.copy(Blocks.GRANITE));
+    public static final Block DOLOMITE = new Block(FabricBlockSettings.copy(Blocks.DEEPSLATE));
+    public static final Block LIMESTONE = new Block(FabricBlockSettings.copy(Blocks.GRANITE));
+    public static final Block LAKE_MISSOULA_CLAYSTONE_SEDIMENT = new Block(FabricBlockSettings.copy(Blocks.GRANITE));
+public static final Block ALKALI_FELDSPAR_GRANITE = new Block(FabricBlockSettings.copy(Blocks.GRANITE));
+public static final Block RHYODACITE =new Block(FabricBlockSettings.copy(Blocks.GRANITE));
+public static final Block NEPHELINE = new Block(FabricBlockSettings.copy(Blocks.GRANITE));
+public static final Block GREY_DACITE = new Block(FabricBlockSettings.copy(Blocks.GRANITE));
+public static final Block STONE_SAND = new Block(FabricBlockSettings.copy(Blocks.SAND));
+public static final Block BOREAL_DIRT = new Block(FabricBlockSettings.copy(Blocks.DIRT));
+public static final Block PEARL_DACITE = new Block(FabricBlockSettings.copy(Blocks.GRANITE));
+public static final Block LAYERSTONE = new Block(FabricBlockSettings.copy(Blocks.STONE));
+    public static final Block GOLD_ORE_LAYERSTONE = new Block(FabricBlockSettings.copy(Blocks.STONE));
+    public static final Block PUMICE = new Block(FabricBlockSettings.copy(Blocks.STONE));
+    public static final Block GRANODIORITE = new Block(FabricBlockSettings.copy(Blocks.STONE));
+public static final Block COMPRESSED_STONE_PEBBLE = new Block(FabricBlockSettings.copy(Blocks.GRANITE));
+public static final Block RHYOLITE_PEBBLE = new Block(FabricBlockSettings.copy(Blocks.GRANITE));
+public static final Block MARBLE_PEBBLE = new Block(FabricBlockSettings.copy(Blocks.GRANITE));
+public static final Block DOLOMITE_PEBBLE = new Block(FabricBlockSettings.copy(Blocks.GRANITE));
+public static final Block LIMESTONE_PEBBLE = new Block(FabricBlockSettings.copy(Blocks.GRANITE));
+public static final Block LAKE_MISSOULA_CLAYSTONE_SEDIMENT_PEBBLE = new Block(FabricBlockSettings.copy(Blocks.GRANITE));
+public static final Block ALKALI_FELDSPAR_GRANITE_PEBBLE = new Block(FabricBlockSettings.copy(Blocks.GRANITE));
+public static final Block RHYODACITE_PEBBLE = new Block(FabricBlockSettings.copy(Blocks.GRANITE));
+public static final Block NEPHELINE_PEBBLE = new Block(FabricBlockSettings.copy(Blocks.GRANITE));
+public static final Block GREY_DACITE_PEBBLE = new Block(FabricBlockSettings.copy(Blocks.GRANITE));
+public static final Block STONE_SAND_PEBBLE = new Block(FabricBlockSettings.copy(Blocks.GRANITE));
+public static final Block PEARL_DACITE_PEBBLE = new Block(FabricBlockSettings.copy(Blocks.GRANITE));
+public static final Block LAYERSTONE_PEBBLE = new Block(FabricBlockSettings.copy(Blocks.GRANITE));
+public static final Block GOLD_ORE_LAYERSTONE_PEBBLE = new Block(FabricBlockSettings.copy(Blocks.GRANITE));
+public static final Block PUMICE_PEBBLE = new Block(FabricBlockSettings.copy(Blocks.GRANITE));
+public static final Block GRANODIORITE_PEBBLE = new Block(FabricBlockSettings.copy(Blocks.GRANITE));
 //Follow the template below if you want to contribute a block, im going to create the BlockItem
-public static final Block EXAMPLE_BLOCK = new Block(FabricBlockSettings.copy(Blocks.GRANITE).requiresTool());
+public static final Block EXAMPLE_BLOCK = new Block(FabricBlockSettings.copy(Blocks.GRANITE));
+
     public static void register() {
         Registry.register(Registry.BLOCK, Utils.ID("compressed_stone"), COMPRESSED_STONE);
         Registry.register(Registry.BLOCK, Utils.ID("rhyolite"), RHYOLITE);
@@ -47,6 +64,22 @@ public static final Block EXAMPLE_BLOCK = new Block(FabricBlockSettings.copy(Blo
         Registry.register(Registry.BLOCK, Utils.ID("gold_ore_layerstone"), GOLD_ORE_LAYERSTONE);
         Registry.register(Registry.BLOCK,Utils.ID("pumice"), PUMICE);
         Registry.register(Registry.BLOCK, Utils.ID("granodiorite"), GRANODIORITE);
+        Registry.register(Registry.BLOCK, Utils.ID("compressed_stone_pebble"), COMPRESSED_STONE_PEBBLE);
+        Registry.register(Registry.BLOCK, Utils.ID("rhyolite_pebble"), RHYOLITE_PEBBLE);
+        Registry.register(Registry.BLOCK, Utils.ID("marble_pebble"), MARBLE_PEBBLE);
+        Registry.register(Registry.BLOCK, Utils.ID("dolomite_pebble"), DOLOMITE_PEBBLE);
+        Registry.register(Registry.BLOCK, Utils.ID("limestone_pebble"), LIMESTONE_PEBBLE);
+        Registry.register(Registry.BLOCK, Utils.ID("lake_missoula_claystone_sediment_pebble"), LAKE_MISSOULA_CLAYSTONE_SEDIMENT_PEBBLE);
+        Registry.register(Registry.BLOCK, Utils.ID("alkali_feldspar_granite_pebble"), ALKALI_FELDSPAR_GRANITE_PEBBLE);
+        Registry.register(Registry.BLOCK, Utils.ID("rhyodacite_pebble"), RHYODACITE_PEBBLE);
+        Registry.register(Registry.BLOCK, Utils.ID("nepheline_pebble"), NEPHELINE_PEBBLE);
+        Registry.register(Registry.BLOCK, Utils.ID("grey_dacite_pebble"), GREY_DACITE_PEBBLE);
+        Registry.register(Registry.BLOCK, Utils.ID("stone_sand_pebble"), STONE_SAND_PEBBLE);
+        Registry.register(Registry.BLOCK, Utils.ID("layerstone_pebble"), LAYERSTONE_PEBBLE);
+        Registry.register(Registry.BLOCK, Utils.ID("pearl_dacite_pebble"), PEARL_DACITE_PEBBLE);   
+        Registry.register(Registry.BLOCK, Utils.ID("gold_ore_layerstone_pebble"), GOLD_ORE_LAYERSTONE_PEBBLE);
+        Registry.register(Registry.BLOCK,Utils.ID("pumice_pebble"), PUMICE_PEBBLE);
+        Registry.register(Registry.BLOCK, Utils.ID("granodiorite_pebble"), GRANODIORITE_PEBBLE);
           //Follow the template below to register an block
         Registry.register(Registry.BLOCK, Utils.ID("example_block"), EXAMPLE_BLOCK);
     }
--- src/main/java/net/glitchyystudios/better_underground/registry/ItemRegistry.java
+++ src/main/java/net/glitchyystudios/better_underground/registry/ItemRegistry.java
@@ -27,6 +27,23 @@ public class ItemRegistry {
     public static final Item GOLD_ORE_LAYERSTONE = new BlockItem(BlockRegistry.GOLD_ORE_LAYERSTONE, new FabricItemSettings().group(BUItemGroups.REALISTICS_GEOLOGICAL_BLOCKS));
     public static final Item PUMICE = new BlockItem(BlockRegistry.PUMICE, new FabricItemSettings().group(BUItemGroups.REALISTICS_GEOLOGICAL_BLOCKS));
     public static final Item GRANODIORITE = new BlockItem(BlockRegistry.GRANODIORITE, new FabricItemSettings().group(BUItemGroups.REALISTICS_GEOLOGICAL_BLOCKS));
+    public static final Item COMPRESSED_STONE_PEBBLE = new BlockItem(BlockRegistry.COMPRESSED_STONE_PEBBLE, new FabricItemSettings().group(BUItemGroups.REALISTICS_GEOLOGICAL_BLOCKS));
+    public static final Item RHYOLITE_PEBBLE = new BlockItem(BlockRegistry.RHYOLITE_PEBBLE, new FabricItemSettings().group(BUItemGroups.REALISTICS_GEOLOGICAL_BLOCKS));
+    public static final Item MARBLE_PEBBLE = new BlockItem(BlockRegistry.MARBLE_PEBBLE, new FabricItemSettings().group(BUItemGroups.REALISTICS_GEOLOGICAL_BLOCKS));
+    public static final Item DOLOMITE_PEBBLE = new BlockItem(BlockRegistry.DOLOMITE_PEBBLE, new FabricItemSettings().group(BUItemGroups.REALISTICS_GEOLOGICAL_BLOCKS));
+    public static final Item LIMESTONE_PEBBLE = new BlockItem(BlockRegistry.LIMESTONE_PEBBLE, new FabricItemSettings().group(BUItemGroups.REALISTICS_GEOLOGICAL_BLOCKS));
+    public static final Item LAKE_MISSOULA_CLAYSTONE_SEDIMENT_PEBBLE = new BlockItem(BlockRegistry.LAKE_MISSOULA_CLAYSTONE_SEDIMENT_PEBBLE, new FabricItemSettings().group(BUItemGroups.REALISTICS_GEOLOGICAL_BLOCKS));
+    public static final Item ALKALI_FELDSPAR_GRANITE_PEBBLE = new BlockItem(BlockRegistry.ALKALI_FELDSPAR_GRANITE_PEBBLE, new FabricItemSettings().group(BUItemGroups.REALISTICS_GEOLOGICAL_BLOCKS));
+    public static final Item RHYODACITE_PEBBLE = new BlockItem(BlockRegistry.RHYODACITE_PEBBLE, new FabricItemSettings().group(BUItemGroups.REALISTICS_GEOLOGICAL_BLOCKS));
+    public static final Item NEPHELINE_PEBBLE = new BlockItem(BlockRegistry.NEPHELINE_PEBBLE, new FabricItemSettings().group(BUItemGroups.REALISTICS_GEOLOGICAL_BLOCKS));
+    public static final Item GREY_DACITE_PEBBLE = new BlockItem(BlockRegistry.GREY_DACITE_PEBBLE, new FabricItemSettings().group(BUItemGroups.REALISTICS_GEOLOGICAL_BLOCKS));
+    public static final Item STONE_SAND_PEBBLE = new BlockItem(BlockRegistry.STONE_SAND_PEBBLE, new FabricItemSettings().group(BUItemGroups.REALISTICS_GEOLOGICAL_BLOCKS));
+    public static final Item PEARL_DACITE_PEBBLE = new BlockItem(BlockRegistry.PEARL_DACITE_PEBBLE, new FabricItemSettings().group(BUItemGroups.REALISTICS_GEOLOGICAL_BLOCKS));
+    public static final Item LAYERSTONE_PEBBLE = new BlockItem(BlockRegistry.LAYERSTONE_PEBBLE, new FabricItemSettings().group(BUItemGroups.REALISTICS_GEOLOGICAL_BLOCKS));
+    public static final Item GOLD_ORE_LAYERSTONE_PEBBLE = new BlockItem(BlockRegistry.GOLD_ORE_LAYERSTONE_PEBBLE, new FabricItemSettings().group(BUItemGroups.REALISTICS_GEOLOGICAL_BLOCKS));
+    public static final Item PUMICE_PEBBLE = new BlockItem(BlockRegistry.PUMICE_PEBBLE, new FabricItemSettings().group(BUItemGroups.REALISTICS_GEOLOGICAL_BLOCKS));
+    public static final Item GRANODIORITE_PEBBLE = new BlockItem(BlockRegistry.GRANODIORITE_PEBBLE, new FabricItemSettings().group(BUItemGroups.REALISTICS_GEOLOGICAL_BLOCKS));
+
 
     public static void register() {
         Registry.register(Registry.ITEM, Utils.ID("compressed_stone"), COMPRESSED_STONE);
@@ -45,6 +62,22 @@ public class ItemRegistry {
         Registry.register(Registry.ITEM, Utils.ID("gold_ore_layerstone"), GOLD_ORE_LAYERSTONE);
         Registry.register(Registry.ITEM, Utils.ID("pumice"), PUMICE);
         Registry.register(Registry.ITEM, Utils.ID("granodiorite"), GRANODIORITE);
+        Registry.register(Registry.ITEM, Utils.ID("compressed_stone_pebble"), COMPRESSED_STONE_PEBBLE);
+        Registry.register(Registry.ITEM, Utils.ID("rhyolite_pebble"), RHYOLITE_PEBBLE);
+        Registry.register(Registry.ITEM, Utils.ID("marble_pebble"), MARBLE_PEBBLE);
+        Registry.register(Registry.ITEM, Utils.ID("dolomite_pebble"), DOLOMITE_PEBBLE);
+        Registry.register(Registry.ITEM, Utils.ID("limestone_pebble"), LIMESTONE_PEBBLE);
+        Registry.register(Registry.ITEM, Utils.ID("lake_missoula_claystone_sediment_pebble"), LAKE_MISSOULA_CLAYSTONE_SEDIMENT_PEBBLE);
+        Registry.register(Registry.ITEM, Utils.ID("alkali_feldspar_granite_pebble"), ALKALI_FELDSPAR_GRANITE_PEBBLE);
+        Registry.register(Registry.ITEM, Utils.ID("rhyodacite_pebble"), RHYODACITE_PEBBLE);
+        Registry.register(Registry.ITEM, Utils.ID("nepheline_pebble"), NEPHELINE_PEBBLE);
+        Registry.register(Registry.ITEM, Utils.ID("grey_dacite_pebble"), GREY_DACITE_PEBBLE);
+        Registry.register(Registry.ITEM, Utils.ID("stone_sand_pebble"), STONE_SAND_PEBBLE);
+        Registry.register(Registry.ITEM, Utils.ID("layerstone_pebble"), LAYERSTONE_PEBBLE);
+        Registry.register(Registry.ITEM, Utils.ID("pearl_dacite_pebble"), PEARL_DACITE_PEBBLE);   
+        Registry.register(Registry.ITEM, Utils.ID("gold_ore_layerstone_pebble"), GOLD_ORE_LAYERSTONE_PEBBLE);
+        Registry.register(Registry.ITEM,Utils.ID("pumice_pebble"), PUMICE_PEBBLE);
+        Registry.register(Registry.ITEM, Utils.ID("granodiorite_pebble"), GRANODIORITE_PEBBLE);
         //use the code below to register an item, rename the item to the name of your item
         Registry.register(Registry.ITEM, Utils.ID("stone_sand"), STONE_SAND);
     }
